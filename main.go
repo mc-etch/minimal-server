7461716a7b8c61e15e748234f8a01d9c08459570
mc-etch <rodacoram@protonmail.com>
2023-01-28 13:27:31 -0700

    sending StatusOK

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-func HandleHello(w http.ResponseWriter, _ *http.Request) {
-	w.Write([]byte("Hello\n"))
+func HandleHello(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hello\n vars from server: %v\n", vars)
 }
 
 func main() {
